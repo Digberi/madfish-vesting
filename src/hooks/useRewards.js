4d99e2a201eaad994e6b0a18e004641c973f299e
letier3110 <38981658+letier3110@users.noreply.github.com>
2022-06-20 13:31:17 +0100

    remove redundant code

--- src/hooks/useRewards.js
+++ src/hooks/useRewards.js
@@ -9,29 +9,13 @@ export const [UseRewardsProvider, useRewards] = constate(() => {
   const [loading, setLoading] = useState(true);
 
   const loadRewards = useCallback(async () => {
-    // if (!storage || !storage.vestings_counter || !pkh) {
     if (!storage || !storage.vestings_counter) {
       setLoading(true);
-      // if (!pkh) setRewards([]);
       return;
     }
     const limit = storage.vestings_counter;
     const newRewards = await loadFromTzktBigmap(storage.vestings.id.toNumber(), limit);
     const arr = newRewards.map((_, index) => ({ ..._.value, id: index }))
-    console.log(arr)
-    // let arr = [];
-    // for (let i = 0; i < limit; i++) {
-    //   const vesting = await storage.vestings.get([i]);
-    //   arr.push(vesting);
-    // }
-    // arr = arr
-    //   .map((_, index) => ({ ..._, id: index }))
-    // // if (pkh) {
-    // //   // arr = arr
-    // //   // .map((_, index) => ({ ..._, id: index }))
-    // //   // .filter((x) => x.receiver === pkh || x.admin !== pkh); // for debug
-    // //   // .filter((x) => x.receiver === pkh || x.admin === pkh);
-    // // }
     setRewards(arr);
     setLoading(false);
   }, [storage]);
